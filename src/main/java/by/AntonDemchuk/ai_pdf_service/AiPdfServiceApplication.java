9accdbf63ba7397aef2d66a4c694795e4bbf94d2
Anton <antond200614@gmail.com>
2026-03-19 09:17:36 +0100

    Init project

---
+++ src/main/java/by/AntonDemchuk/ai_pdf_service/AiPdfServiceApplication.java
@@ -0,0 +1,13 @@
+package by.AntonDemchuk.ai_pdf_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPdfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPdfServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/AntonDemchuk/ai_pdf_service/AiPdfServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.AntonDemchuk.ai_pdf_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPdfServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
