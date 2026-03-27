fdc40af01ed1b1cf5358f80985c2eeb74015aedc
Anton <antond200614@gmail.com>
2026-03-27 13:25:43 +0100

    add: aws s3 setup

---
+++ src/main/java/by/AntonDemchuk/ai_pdf_service/service/S3Service.java
@@ -0,0 +1,29 @@
+package by.AntonDemchuk.ai_pdf_service.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
+    private final S3Client s3Client;
+
+    public void uploadFile(MultipartFile file) throws IOException {
+        s3Client.putObject(PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(file.getOriginalFilename())
+                .build(),
+                RequestBody.fromBytes(file.getBytes()));
+    }
+}
